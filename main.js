9f5458c53518411d87de94d20f7160729783c335
HasiniPrasadika <gkhpmadhushani17@gmail.com>
2024-05-01 13:38:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Computer Engineering Undergraduate" , "Frontend Developer" , "Web Developer" , "Mobile Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
